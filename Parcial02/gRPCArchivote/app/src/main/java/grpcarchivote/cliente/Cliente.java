afbb91309847d337e4175008e68ed2e3a5a41bf9
Ale_Pucheta <aavp0316@gmail.com>
2024-04-21 19:58:30 -0600

    Archivote gRPC Streaming part2

--- Parcial02/gRPCArchivote/app/src/main/java/grpcarchivote/cliente/Cliente.java
+++ Parcial02/gRPCArchivote/app/src/main/java/grpcarchivote/cliente/Cliente.java
@@ -1,82 +1,33 @@
 package grpcarchivote.cliente;
 
-import com.proto.enviar.EnviarArchivo.ArchivoChunk;
 import com.proto.enviar.EnviarArchivo.ArchivoRequest;
 import com.proto.enviar.ArchivoServiceGrpc;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.stub.StreamObserver;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 
 public class Cliente {
+
     public static void main(String[] args) {
         String host = "localhost";
         int puerto = 8080;
 
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(host, puerto)
-                .usePlaintext()
-                .build();
+        ManagedChannel ch = ManagedChannelBuilder.forAddress(host, puerto).usePlaintext().build();
 
-        ArchivoServiceGrpc.ArchivoServiceStub stub = ArchivoServiceGrpc.newStub(channel);
+       
+        lecturaStream(ch);
 
-        FileOutputStream outputStream = null;
-
-        try {
-            outputStream = new FileOutputStream("archivote.csv"); // Nombre del archivo de salida
-            
-            StreamObserver<ArchivoChunk> responseObserver = new StreamObserver<ArchivoChunk>() {
-                @Override
-                public void onNext(ArchivoChunk value) {
-                    try {
-                        outputStream.write(value.getChunk().toByteArray());
-                    } catch (IOException e) {
-                        onError(e);
-                    }
-                }
-
-                @Override
-                public void onError(Throwable t) {
-                    handleError(t);
-                }
-
-                @Override
-                public void onCompleted() {
-                    try {
-                        outputStream.close();
-                        System.out.println("Archivo recibido correctamente.");
-                    } catch (IOException e) {
-                        onError(e);
-                    }
-                }
-            };
-
-            StreamObserver<ArchivoRequest> requestObserver = stub.enviarArchivo(responseObserver);
-
-            ArchivoRequest request = ArchivoRequest.newBuilder().setNombreArchivo("archivote.csv").build();
-            requestObserver.onNext(request);
-            requestObserver.onCompleted();
-
-            // Espera a que la transferencia del archivo se complete
-            Thread.sleep(5000);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            if (outputStream != null) {
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            channel.shutdown();
-        }
+        System.out.println("Apagando...");
+        ch.shutdown();
     }
 
-    private static void handleError(Throwable t) {
-        System.err.println("Error en la comunicación con el servidor: " + t.getMessage());
+    public static void lecturaStream(ManagedChannel ch){
+        ArchivoServiceGrpc.ArchivoServiceBlockingStub stub = ArchivoServiceGrpc.newBlockingStub(ch);
+        ArchivoRequest peticion = ArchivoRequest.newBuilder().setArchivo("/archivote.csv").build();
+
+        stub.enviarArchivo(peticion).forEachRemaining(respuesta ->{
+            System.out.println("Respuesta RPC: " + respuesta.getCadena());
+        });
     }
 }
--- Parcial02/gRPCArchivote/app/src/main/java/grpcarchivote/servidor/ServidorImpl.java
+++ Parcial02/gRPCArchivote/app/src/main/java/grpcarchivote/servidor/ServidorImpl.java
@@ -1,38 +1,25 @@
 package grpcarchivote.servidor;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.Scanner;
 
 import com.proto.enviar.EnviarArchivo.ArchivoChunk;
 import com.proto.enviar.EnviarArchivo.ArchivoRequest;
-import com.google.protobuf.ByteString;
 import com.proto.enviar.ArchivoServiceGrpc;
 
 import io.grpc.stub.StreamObserver;
 
+
 public class ServidorImpl extends ArchivoServiceGrpc.ArchivoServiceImplBase {
     @Override
     public void enviarArchivo(ArchivoRequest request, StreamObserver<ArchivoChunk> responseObserver) {
-        try {
-            String nombreArchivo = request.getNombreArchivo();
-            InputStream inputStream = ServidorImpl.class.getResourceAsStream("/" + nombreArchivo);
-            if (inputStream == null) {
-                throw new IOException("Archivo no encontrado: " + nombreArchivo);
-            }
-
-            byte[] buffer = new byte[1024]; // Tamaño del chunk
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                byte[] chunk = new byte[bytesRead];
-                System.arraycopy(buffer, 0, chunk, 0, bytesRead);
-                ArchivoChunk archivoChunk = ArchivoChunk.newBuilder().setChunk(ByteString.copyFrom(chunk)).build();
-                responseObserver.onNext(archivoChunk);
+        try (Scanner scanner = new Scanner(ServidorImpl.class.getResourceAsStream(request.getArchivo()))) {
+            while (scanner.hasNextLine()) {
+                ArchivoChunk cadena = ArchivoChunk.newBuilder().setCadena(scanner.nextLine()).build();
+                responseObserver.onNext(cadena);
             }
 
             responseObserver.onCompleted();
-            inputStream.close();
-        } catch (IOException e) {
-            responseObserver.onError(e);
         }
     }
+    
 }
